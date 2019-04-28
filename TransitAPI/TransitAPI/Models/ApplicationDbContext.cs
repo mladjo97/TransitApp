e2c863bf7c700e553d5de88d225a932b74f3690c
Mladen Milosevic <mldnmilosevic@gmail.com>
2019-04-28 12:21:14 +0200

    Added Entity database connection to WebApi and Bootstrap 4 to Angular

---
+++ TransitAPI/TransitAPI/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace TransitAPI.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("TransitAppDbConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
